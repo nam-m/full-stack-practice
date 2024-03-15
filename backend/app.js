6d1fbfc357f419740c7ef5edcdae9e8e915820a2
Nam Anh Mai <maithenamanh@gmail.com>
2024-03-15 00:45:01 -0700

    Add user model and router

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
 
 mongoose.set('strictQuery', false)
 
@@ -26,6 +28,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
 // handler of requests with unknown endpoint
 app.use(middleware.unknownEndpoint)
