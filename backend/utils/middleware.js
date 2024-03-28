7c192bb823ad4e4cae78e0b276c3506be156243b
Nam Anh Mai <maithenamanh@gmail.com>
2024-03-28 14:51:42 -0700

    Add error handling for jwt and uniqueness check for user

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -16,10 +16,17 @@ const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
+  }
+  else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-  } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+  }
+  // Add condition to check unique user 'username'
+  else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
     return response.status(400).json({ error: 'expected `username` to be unique' })
+  }
+  // Add condition to check user token
+  else if (error.name === 'JsonWebTokenError') {
+    return response.status(400).json({ error: 'token missing or invalid' })
   }
   next(error)
 }
