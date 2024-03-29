0ba33c339110414185ea0c77183313d855173818
Nam Anh Mai <maithenamanh@gmail.com>
2024-03-28 23:02:05 -0700

    Add login and note form components to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import loginService from './services/login'
+import logoutService from './services/logout'
+import LoginForm from './components/LoginForm'
+import Togglable from './components/Togglable'
+import NoteForm from './components/NoteForm'
 
 const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -15,23 +22,22 @@ const App = () => {
       })
   }, [])
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5,
+
+  useEffect(() => {
+    const loggedUserJSON = localStorage.getItem('loggedNoteUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
     }
-  
+  }, [])
+
+  const addNote = (noteObject) => {
     noteService
       .create(noteObject)
       .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
-        setNewNote('')
       })
-  }
-
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
   }
 
   const toggleImportanceOf = id => {
@@ -53,9 +59,62 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
+  const handleLogin = async (username, password) => {
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      // Save user to local storage to persist on specific browser
+      localStorage.setItem(
+        'loggedNoteUser', JSON.stringify(user)
+      )
+      // Set token to authorize user's note manipulation
+      noteService.setToken(user.token)
+      setUser(user)
+    } catch(exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  // Currently, logout is only hanlded on client side
+  // by removing the user and their token from localStorage
+  // Server side token invalidation techniques such as blacklisting, timeout 
+  // may be considered in the future
+  const handleLogout = () => {
+    try {
+      // await logoutService.logout()
+      localStorage.removeItem('loggedNoteUser')
+      setUser(null)
+    } catch (exception) {
+      setErrorMessage('cannot logout')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
+
+      {user === null ? 
+        <Togglable buttonLabel="log in">
+          <LoginForm handleLogin={handleLogin}/>
+        </Togglable> :
+        <div>
+          <p>{user.name} logged-in</p>
+          <Togglable buttonLabel="new note">
+            <NoteForm createNote={addNote}/>
+          </Togglable>
+          
+          <button onClick={() => handleLogout()}>log out</button>
+        </div>
+      }
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -70,13 +129,6 @@ const App = () => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-      <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form> 
     </div>
   )
 }
---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+const LoginForm = (props) => {
+  const [username, setUsername] = useState('')   
+  const [password, setPassword] = useState('') 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.handleLogin(username, password)
+    setUsername('')
+    setPassword('')
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          username
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+      </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+ )
+}
+
+export default LoginForm
